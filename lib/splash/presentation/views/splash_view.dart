78d142a4fa9229d4d5550d55d9def5dc86bed353
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-12-12 14:44:15 +0200

    create class app_assets

--- lib/splash/presentation/views/splash_view.dart
+++ lib/splash/presentation/views/splash_view.dart
@@ -6,8 +6,10 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SplashViewBody(),
+    return const SafeArea(
+      child: Scaffold(
+        body: SplashViewBody(),
+      ),
     );
   }
 }
---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,27 @@
+abstract class AppAssets {
+  static const String bitCoin = "assets/images/bitcoin.gif";
+
+  static const String io = "assets/images/io.png";
+
+  static const String loading = "assets/images/loading.gif";
+
+  static const String photo = "assets/images/photo.PNG";
+
+  static const String iconHomeOutlined = "assets/icons/1.1.png";
+
+  static const String iconHome = "assets/icons/1.2.png";
+
+  static const String iconCurrencyOutlined = "assets/icons/2.1.png";
+
+  static const String iconCurrency = "assets/icons/2.2.png";
+
+  static const String iconNotificationOutlined = "assets/icons/3.1.png";
+
+  static const String iconNotification = "assets/icons/3.2.png";
+
+  static const String iconPersonOutlined = "assets/icons/4.1.png";
+
+  static const String iconPerson = "assets/icons/4.2.png";
+
+  static const String icon = "assets/icons/5.1.png";
+}
