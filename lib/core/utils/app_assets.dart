1698c23eaf5fc0b896586d6172d7516876d77083
Crypto_App <mahmoudsaber223015email@gmail.com>
2023-12-25 15:24:23 +0200

    refactor

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -7,21 +7,7 @@ abstract class AppAssets {
 
   static const String photo = "assets/images/photo.PNG";
 
-  static const String iconHomeOutlined = "assets/icons/1.1.png";
-
-  static const String iconHome = "assets/icons/1.2.png";
-
-  static const String iconCurrencyOutlined = "assets/icons/2.1.png";
-
-  static const String iconCurrency = "assets/icons/2.2.png";
-
   static const String iconNotificationOutlined = "assets/icons/3.1.png";
-
-  static const String iconNotification = "assets/icons/3.2.png";
-
-  static const String iconPersonOutlined = "assets/icons/4.1.png";
-
-  static const String iconPerson = "assets/icons/4.2.png";
 
   static const String icon = "assets/icons/5.1.png";
 }
